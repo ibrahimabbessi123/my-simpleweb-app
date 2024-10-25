58cd0a41f82bf678ce8b48ed779d1b75e4d276b2
ibrahimabbessi123 <abbessiibrahim123@gmail.com>
2024-10-25 11:33:40 +0100

    Update build-app.sh

--- build-app.sh
+++ build-app.sh
@@ -3,7 +3,7 @@
 BUILD_DIR="dist"
 # Supprimer le dossier de build s'il existe déjà
 if [ -d "$BUILD_DIR" ]; then
-rm -rf "$BUILD_DIR"
+  rm -rf "$BUILD_DIR"
 fi
 # Créer un nouveau dossier de build
 echo "Créer le dossier de build DIST"
@@ -18,7 +18,7 @@ cp build-app.sh "$BUILD_DIR/"
 # Compresser les fichiers HTML, CSS et JS
 echo "Compression des fichiers..."
 for file in index.html script.js style.css; do
-gzip -c "$file" > "$BUILD_DIR/${file}.gz"
+  gzip -c "$file" > "$BUILD_DIR/${file}.gz"
 done
 docker build -t image-from-jenkins .
 docker run -d -p 2020:8080 --name container-from-jenkins image-from-jenkins
